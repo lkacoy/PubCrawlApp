9473ecb8802b6cdb5d0948eb49cb1ee64d3baa93
crocomoose <lorenzogarch@gmail.com>
2015-04-08 11:54:53 -0400

    Test

---
+++ PubCrawl/src/com/example/pubcrawl/PubCrawlMain.java
@@ -0,0 +1,15 @@
+package com.example.pubcrawl;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PubCrawlMain extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pub_crawl_main);
+	}
+}
